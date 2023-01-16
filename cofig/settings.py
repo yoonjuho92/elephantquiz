9532a6c115d6a761ad5f9a235084cce69a08a788
yoonjuho92 <yoonjuho92@gmail.com>
2023-01-16 20:49:34 +0900

    allowed_host change2

--- cofig/settings.py
+++ cofig/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-_sv4f82+f0=ph0x!evhi4vf%_jl%^br%!&3x^9fgd_zwq1^=iv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [3.38.51.38]
+ALLOWED_HOSTS = ['3.38.51.38']
 
 
 # Application definition
